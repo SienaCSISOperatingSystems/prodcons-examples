d1ed8496e60093a2c9ac3173a9fa0db201211f46
Jim Teresco <terescoj@gmail.com>
2020-09-14 08:57:14 -0400

    Added sys/wait.h

--- shmem/prodcons-shmem-counter.c
+++ shmem/prodcons-shmem-counter.c
@@ -11,6 +11,7 @@
 */
 
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
--- shmem/prodcons-shmem-oneempty.c
+++ shmem/prodcons-shmem-oneempty.c
@@ -10,6 +10,7 @@
 */
 
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
